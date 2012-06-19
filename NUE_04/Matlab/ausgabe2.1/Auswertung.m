6d982f8612039ae4fd180d8d65d804f845e8edd8
Boris Henckell <borishenckell@gmail.com>
2012-06-19 09:37:30 +0200

    kleine Verbesserungen

--- NUE_04/Matlab/ausgabe2.1/Auswertung.m
+++ NUE_04/Matlab/ausgabe2.1/Auswertung.m
@@ -1,26 +1,26 @@
 %Auswertung
 clear;clc;close all;
 %% einlesen der gemessenen Daten
-a = load('shape-top-tp-recht-a02_10kHz+rekon');
-b = load('shape-top-tp-recht-a05_10kHz+rekon');
-c = load('shape-top-tp-recht-a07_10kHz+rekon');
-d = load('shape-top-tp-recht-a02_20kHz+rekon');
-e = load('shape-top-tp-recht-a05_20kHz+rekon');
-g = load('shape-top-tp-recht-a07_20kHz+rekon');
+a = load('shape-top-sin-a02_10kHz+rekon');
+b = load('shape-top-sin-a05_10kHz+rekon');
+c = load('shape-top-sin-a07_10kHz+rekon');
+d = load('shape-top-sin-a02_20kHz+rekon');
+e = load('shape-top-sin-a05_20kHz+rekon');
+g = load('shape-top-sin-a07_20kHz+rekon');
 %% Konstanten, Achsen etc
 
 f = 2000; %Quellsignal mit 2kHz
 
 %% plotten des Zeitsignals
 % PAM moduliert
-Zeitplot2(a,b,c,d,e,g, 'Sinus', 'Shape_Top', 'A',1)
+Zeitplot(a,b,c,d,e,g, 'Sinus', 'Shape_Top', 'A',1)
 
 % PAM demoduliert
 % Zeitplot2(a,b,c,d,e,g, 'Sinus', 'Shape_Top', 'B',2)
 
 %% Plotten des frequenzsignals
 
-% FFTshiftplotZP6subplots(a, 'b', 3)
+ FFTshiftplotZP6subplots(a, b, c, d, e, g,'b', 3)
 
 
 %% infos
--- NUE_04/Matlab/ausgabe2.1/FFTshiftplotZP6subplots.m
+++ NUE_04/Matlab/ausgabe2.1/FFTshiftplotZP6subplots.m
@@ -1,4 +1,4 @@
-function FFTshiftplotZP6subplots(a,farbe, fignum)
+function FFTshiftplotZP6subplots(a,b,c,d,e,g,farbe, fignum)
 %% Infos
 % y         - Zeitvektor
 % f_T       - Abtastfrequenz
@@ -17,24 +17,100 @@ function FFTshiftplotZP6subplots(a,farbe, fignum)
 % 
 % T_ges = T_ges * zpf;
 
-
-
-%Berechnung des Spektrums
-a_DFT = fftshift(fft(a.B));
-%y_DFT = fftshift(y_DFT);
-N = length(a.B);
-%Betragsspektrum
-%y_DFT_abs = 10*LOG10(abs(y_DFT)/N);
-a_DFT_abs = abs(a_DFT)/N;
-%Frequenzachse
-f_T = 1/a.Tinterval;
-f_DFT = f_T/(N-1)*((-N/2):(N/2-1));
-
 %% plot
 figure(fignum);
 
 subplot(2,3,1);
-    plot(f_DFT,a_DFT_abs);
+    %Berechnung des Spektrums
+    a_DFT = fftshift(fft(a.A));
+    Na = length(a.A);
+    %Betragsspektrum
+    a_DFT_abs = 10*LOG10(abs(a_DFT)/Na);
+    %Frequenzachse
+    f_Ta = 1/a.Tinterval;
+    f_DFTa = f_Ta/(Na-1)*((-Na/2):(Na/2-1));
+
+    plot(f_DFTa,a_DFT_abs);
+%    axis tight;
+%    AXIS(-1*10^6 1*10^6 0 5*10^-3])
+    title('Amplitudenspektrum');
+    xlabel('f/Hz');
+    ylabel('A(f)[dB]');
+subplot(2,3,2);
+    %Berechnung des Spektrums
+    b_DFT = fftshift(fft(b.A));
+    Nb = length(b.A);
+    %Betragsspektrum
+    b_DFT_abs = 10*LOG10(abs(b_DFT)/Nb);
+    %Frequenzachse
+    f_Tb = 1/b.Tinterval;
+    f_DFTb = f_Tb/(Nb-1)*((-Nb/2):(Nb/2-1));
+
+    plot(f_DFTb,b_DFT_abs);
+%    axis tight;
+%    AXIS(-1*10^6 1*10^6 0 5*10^-3])
+    title('Amplitudenspektrum');
+    xlabel('f/Hz');
+    ylabel('A(f)[dB]');
+subplot(2,3,3);
+    %Berechnung des Spektrums
+    c_DFT = fftshift(fft(c.A));
+    Nc = length(c.A);
+    %Betragsspektrum
+    c_DFT_abs = 10*LOG10(abs(c_DFT)/Nc);
+    %Frequenzachse
+    f_Tc = 1/c.Tinterval;
+    f_DFTc = f_Tc/(Nc-1)*((-Nc/2):(Nc/2-1));
+
+    plot(f_DFTc,c_DFT_abs);
+%    axis tight;
+%    AXIS(-1*10^6 1*10^6 0 5*10^-3])
+    title('Amplitudenspektrum');
+    xlabel('f/Hz');
+    ylabel('A(f)[dB]');
+subplot(2,3,4);
+    %Berechnung des Spektrums
+    d_DFT = fftshift(fft(d.A));
+    Nd = length(d.A);
+    %Betragsspektrum
+    d_DFT_abs = 10*LOG10(abs(d_DFT)/Nd);
+    %Frequenzachse
+    f_Td = 1/d.Tinterval;
+    f_DFTd = f_Td/(Nd-1)*((-Nd/2):(Nd/2-1));
+
+    plot(f_DFTd,d_DFT_abs);
+%    axis tight;
+%    AXIS(-1*10^6 1*10^6 0 5*10^-3])
+    title('Amplitudenspektrum');
+    xlabel('f/Hz');
+    ylabel('A(f)[dB]');
+subplot(2,3,5);
+    %Berechnung des Spektrums
+    e_DFT = fftshift(fft(e.A));
+    Ne = length(e.A);
+    %Betragsspektrum
+    e_DFT_abs = 10*LOG10(abs(e_DFT)/Ne);
+    %Frequenzachse
+    f_Te = 1/e.Tinterval;
+    f_DFTe = f_Te/(Ne-1)*((-Ne/2):(Ne/2-1));
+
+    plot(f_DFTe,e_DFT_abs);
+%    axis tight;
+%    AXIS(-1*10^6 1*10^6 0 5*10^-3])
+    title('Amplitudenspektrum');
+    xlabel('f/Hz');
+    ylabel('A(f)[dB]');
+subplot(2,3,6);
+    %Berechnung des Spektrums
+    g_DFT = fftshift(fft(g.A));
+    Ng = length(g.A);
+    %Betragsspektrum
+    g_DFT_abs = 10*LOG10(abs(g_DFT)/Ng);
+    %Frequenzachse
+    f_Tg = 1/g.Tinterval;
+    f_DFTg = f_Tg/(Ng-1)*((-Ng/2):(Ng/2-1));
+
+    plot(f_DFTg,g_DFT_abs);
 %    axis tight;
 %    AXIS(-1*10^6 1*10^6 0 5*10^-3])
     title('Amplitudenspektrum');
