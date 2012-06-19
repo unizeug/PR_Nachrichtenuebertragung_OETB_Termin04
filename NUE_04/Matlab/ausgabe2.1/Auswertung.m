870193fbeba94c0ff97b412f04924a7116ed0b13
Boris Henckell <borishenckell@gmail.com>
2012-06-19 11:38:31 +0200

    Weiter An der Matlab fequenz datei gearbeitet

--- NUE_04/Matlab/ausgabe2.1/Auswertung.m
+++ NUE_04/Matlab/ausgabe2.1/Auswertung.m
@@ -1,12 +1,12 @@
 %Auswertung
 clear;clc;close all;
 %% einlesen der gemessenen Daten
-a = load('shape-top-tp-recht-a02_10kHz+rekon');
-b = load('shape-top-sin-a05_10kHz+rekon');
-c = load('shape-top-sin-a07_10kHz+rekon');
-d = load('shape-top-sin-a02_20kHz+rekon');
-e = load('shape-top-sin-a05_20kHz+rekon');
-g = load('shape-top-sin-a07_20kHz+rekon');
+a = load('flat-top-tp-recht-a02_10kHz+rekon');
+b = load('flat-top-tp-recht-a05_10kHz+rekon');
+c = load('flat-top-tp-recht-a07_10kHz+rekon');
+d = load('flat-top-tp-recht-a02_20kHz+rekon');
+e = load('flat-top-tp-recht-a05_20kHz+rekon');
+g = load('flat-top-tp-recht-a07_20kHz+rekon');
 %% Konstanten, Achsen etc
 
 f = 2000; %Quellsignal mit 2kHz
